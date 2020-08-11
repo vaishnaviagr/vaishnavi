ac980671153851bbdc6507f90e3683a0a92275a7
vaishnaviagr <65871189+vaishnaviagr@users.noreply.github.com>
2020-08-11 17:04:20 +0530

    Add files via upload

---
+++ imp.py
@@ -0,0 +1,12 @@
+choice=int(input("Enter your choice "))
+
+if choice==1:
+    r=int(input("Enter the radius"))
+    area=3.14*r*r
+    print("area of circle =",area)
+else:
+    r=int(input("Enter the radius"))
+    peri=2*3.14*r
+    print("peimeter of circle =",peri)
+    
+
